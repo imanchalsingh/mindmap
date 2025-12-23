0d46929c36aa54eab11fcfce85684f0d2b8e61c1
imanchalsingh <singhanchal159357@gmail.com>
2025-12-23 15:49:09 +0530

    Commit : Add Hero Section and Added changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import HeroSection from "./Component/HeroSection";
 import InteractiveElement from "./Component/MindMapX";
 
 function App() {
   return (
-    <div>
-      <InteractiveElement />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
+        <Routes>
+          <Route path="/" element={<HeroSection />} />
+          <Route path="/mindmap" element={<InteractiveElement />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
