665d433e91a52e311965b0809ef919cb8b88ab29
imanchalsingh <singhanchal159357@gmail.com>
2025-12-29 16:12:41 +0530

    Fix : Resolve Error and install require pkgs

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    'import.meta.env.VITE_OPENAI_API_KEY': JSON.stringify(process.env.VITE_OPENAI_API_KEY)
+  }
 });
